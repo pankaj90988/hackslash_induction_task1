8ecdd9710e40068dc99982cf22f69cfbbf146732
pankaj90988 <pankajjnv2005@gmail.com>
2024-03-03 16:47:50 +0530

    p

--- hackslashTask1.js
+++ hackslashTask1.js
@@ -1,17 +1,16 @@
 function btn(){
     let currentMode="white";
     if(currentMode=="white"){
-    document.querySelector(".main-container").style.backgroundColor="black";
-    document.querySelector(".main-container").style.color="white";
     document.querySelector("body").style.backgroundColor="black";
+    document.querySelector("body").style.color="white";
+    document.querySelector(".main-container").style.backgroundColor="black";
     document.querySelector("a").style.backgroundColor="white";
     }
     else{
-        document.querySelector(".main-container").style.backgroundColor="white";
+        document.querySelector("body").style.backgroundColor="white";
     document.querySelector(".main-container").style.color="black";
     document.querySelector("body").style.backgroundColor="white";
     }
 }
 
 
-///rgere5g5egeg
