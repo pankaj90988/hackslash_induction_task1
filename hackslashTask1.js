cae649372f331c6229ee14052760b0dac66584da
pankaj90988 <pankajjnv2005@gmail.com>
2024-03-25 17:22:39 +0530

    sixth commit

--- hackslashTask1.js
+++ hackslashTask1.js
@@ -4,6 +4,9 @@ function btn(){
     document.querySelector("body").style.backgroundColor="black";
     document.querySelector("body").style.color="white";
     document.querySelector(".main-container").style.backgroundColor="black";
+    document.querySelector(".main-container").style.color="white";
+    document.querySelector("body").style.backgroundColor="black"; 
+    document.querySelector(".sub-cont-1").style.color="white";
     document.querySelector("a").style.backgroundColor="white";
     }
     else{
